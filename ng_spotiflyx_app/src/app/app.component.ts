208b5bfb78d80438f81ff9bb9f3e1b570773edd1
Saravenpi Shigeturi <saravepi@tuta.io>
2024-04-18 20:40:19 +0200

    [ADD] nav bar

--- ng_spotiflyx_app/src/app/app.component.ts
+++ ng_spotiflyx_app/src/app/app.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SpotiFlyx';
+export class AppComponent implements OnInit {
+  title = 'app';
 
-  changeTitle() {
-    this.title = "test"
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit() {
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    let navbar = this.document.querySelector('.navbar');
+    let searchForm = this.document.querySelector('.search-form');
+
+    this.document.querySelector('#menu-btn')?.addEventListener('click', () => {
+      if (navbar) {
+        navbar.classList.toggle('active');
+      }
+    });
+
+    this.document.querySelector('#search-btn')?.addEventListener('click', () => {
+      if (searchForm) {
+        searchForm.classList.toggle('active');
+      }
+    });
   }
 }
