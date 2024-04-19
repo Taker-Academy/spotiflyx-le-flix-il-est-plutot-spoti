0d698084b70b799cdeb08b9e0d3fc091758a06e8
Jules <juleslordet@gmail.com>
2024-04-19 19:57:17 +0200

    add backend structure file

---
+++ backend/src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "pix_user",
+    password: "pixservice123",
+    database: "pix_reaction_db",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
