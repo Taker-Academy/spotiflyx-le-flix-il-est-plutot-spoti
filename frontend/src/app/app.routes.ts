14c1f75212951c98e54478dc78eddfe4774e2f60
Jules <juleslordet@gmail.com>
2024-04-19 20:22:04 +0200

    UPDATE initial routes, UPDATE frontend file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'login', component: LoginComponent},
+    { path: '', component: LoginComponent},
+    { path: 'home', component: HomeComponent},
     { path: '**', redirectTo: ''},
 ];
