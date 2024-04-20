06de766d3f6ac2643fd0e0bbedf9bc41334ad865
Jules <juleslordet@gmail.com>
2024-04-20 19:08:45 +0200

    [ADD] {frontend} => legal web page connected

--- frontend/src/app/legal/legal.component.ts
+++ frontend/src/app/legal/legal.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-legal',
   templateUrl: './legal.component.html',
   styleUrls: ['./legal.component.css']
 })
-export class LegalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LegalComponent {
+  constructor(private router: Router) { }
+  navigateToRegister() {
+    this.router.navigate(['/register']);
   }
-
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
