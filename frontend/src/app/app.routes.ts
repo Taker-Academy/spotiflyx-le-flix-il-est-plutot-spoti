04d8dac4c3f8f08af5840396a4bbac2d1f3a221d
Saravenpi Shigeturi <saravepi@tuta.io>
2024-04-20 14:38:41 +0200

    [FIX] routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: '**', redirectTo: ''},
