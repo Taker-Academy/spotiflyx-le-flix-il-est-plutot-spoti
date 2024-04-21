9c1dae5b1cc65c1b67604c298d49534204eff18d
Jules <juleslordet@gmail.com>
2024-04-21 15:46:26 +0200

    [DELETE] {backend} => delete save route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,11 +16,6 @@ export const Routes = [{
     controller: UserController,
     action: "one"
 }, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
     method: "delete",
     route: "/users/:id",
     controller: UserController,
