882a23c766d5607cd5a0c88d67cc7107d68d074f
Jules Lordet <jules.lordet@epitech.eu>
2024-04-24 18:27:33 +0200

    [FIX] {backend} => docker compose, change host of back end to localhost to postgres

--- backendDebug/src/data-source.ts
+++ backendDebug/src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "postgres", // Nom du service Docker pour PostgreSQL
     port: 5432,
     username: "pst_user",
     password: "pstservice123",
     database: "pst_db",
-    synchronize: true,
+    synchronize: true, // Attention : à utiliser uniquement en développement
     logging: false,
     entities: [User],
     migrations: [],
     subscribers: [],
-})
+});
