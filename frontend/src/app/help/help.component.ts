4b758c0b567dea67a4e566132a0bfb2f56116f32
Jules Lordet <jules.lordet@epitech.eu>
2024-04-29 14:12:44 +0200

    [ADD] {frontend} => Help component

---
+++ frontend/src/app/help/help.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, ViewChild, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from '../header/header.component';
+
+
+@Component({
+  selector: 'app-help',
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HeaderComponent
+  ],
+  providers: [
+    HttpClientModule,
+    BrowserModule,
+    Router
+  ],
+  templateUrl: './help.component.html',
+  styleUrl: './help.component.css'
+})
+export class HelpComponent {
+  constructor(private router: Router) { }
+}
