e9601ed78b6b516e62fa8c4055b9e2c821f99069
Jules Lordet <jules.lordet@epitech.eu>
2024-04-30 10:36:10 +0200

    [ADD] docker compose (back & front)

--- frontend/server.ts
+++ frontend/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import * as Mailjet from 'node-mailjet';
 
 
 // The Express app is exported so that it can be used by serverless Functions.
